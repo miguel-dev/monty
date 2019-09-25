4b42f1b958d8a2343a0b6f1eccb6d1453a472fbe
miguel-dev <miguelacortes@zoho.com>
2019-09-25 17:15:06 +0000

    [Feature] get_opcode function selecting the right operation ocrresponding to the opcode

---
+++ get_opcode.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * get_opcode - Returns a pointer to the function performing the operation
+ * indicated by the opcode.
+ * @s: String containing the opcode.
+ *
+ * Return: Pointer to a function or NULL if it doesn't exist.
+ */
+void (*get_opcode(char *s))(stack_t **stack, unsigned int line_number)
+{
+	instruction_s opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (opcodes[i].opcode != NULL)
+	{
+		if (*(opcodes[i]).opcode == s)
+			return (opcodes[i].f);
+		i++;
+	}
+	return (NULL);
+}
