7a22129e5cc611774a5ed417e4670fdc962d827d
miguel-dev <miguelacortes@zoho.com>
2019-09-27 22:47:54 +0000

    [Fixes] Checks if the stack is empty

--- add.c
+++ add.c
@@ -11,13 +11,20 @@ void add(stack_t **stack, unsigned int line_number)
 	stack_t *aux1, *aux2;
 	int n = 0, i = 0;
 
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+                error_flag = 1;
+                return;
+	}
+
 	aux1 = *stack;
 	n = aux1->n;
 
 	for (i = 0; aux1; i++)
 		aux1 = aux1->next;
 
-	if (i <= 2)
+	if (i < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		error_flag = 1;
