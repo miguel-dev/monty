004e803f2369eacffabd5db3c8d4b19853bc9bce
miguel-dev <miguelacortes@zoho.com>
2019-09-27 02:37:11 +0000

    [Feature] Task 5

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Doesn't do anything.
+ * @stack: Stack.
+ * @line_number: Number of line.
+ */
+void nop(__attribute__((unused)) stack_t **stack,
+	 __attribute__((unused)) unsigned int line_number)
+{
+
+
+}
