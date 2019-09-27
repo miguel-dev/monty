4d8f83ebe5aea720cbc32b7fe29b6ae58492cfc7
miguel-dev <miguelacortes@zoho.com>
2019-09-27 23:02:29 +0000

    [Fixes] Added prototype of sub

--- monty.h
+++ monty.h
@@ -55,6 +55,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 int is_integer(char *str);
 void free_stack(stack_t *head);
 void _freefunction(char *linenumber, FILE *file);
