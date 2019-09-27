1674a13f232b93bd7ef03ddf10e15224421c3fb0
Yesidh <944@holbertonschool.com>
2019-09-26 23:57:33 -0500

    #Fix: changing condition for more than two nodes

--- swap.c
+++ swap.c
@@ -8,17 +8,21 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 
-	stack_t *aux;
-	int n = 0;
+	stack_t *aux1, *aux2;
+	int n = 0, i = 0;
 
-	if ((*stack)->next->next == NULL)
+	aux1 = *stack;
+	for(i = 0; aux1; i++)
+		aux1 = aux1->next;
+
+	if (i <= 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack to short\n", line_number);
 		error_flag = 1;
 		return;
 	}
-	aux = (*stack)->next;
+	aux2 = (*stack)->next;
 	n = (*stack)->n;
-	(*stack)->n = aux->n;
-	aux->n = n;
+	(*stack)->n = aux2->n;
+	aux2->n = n;
 }
